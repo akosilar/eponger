2fca0969760680d3a2e14102f86df47a6474bd4c
akosilar <55524614+akosilar@users.noreply.github.com>
2022-09-04 18:54:14 -0400

    added redirect and 404 to app.js

--- server/app.js
+++ server/app.js
@@ -9,3 +9,13 @@ app.listen(3000);
 app.get('/', (req,res) => {
     res.sendFile('./client/index.html', {root: "../"});
 });
+
+//redirect from home
+app.get('/home', (req,res) => {
+    res.redirect('/');
+});
+
+//404
+app.use((req,res) => {
+    res.status(404).sendFile('./client/404.html', {root: "../"});
+});
